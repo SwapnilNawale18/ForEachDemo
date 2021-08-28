9080c3cdf0ba924c5157ac876c3373570fc5d7d8
Swapnil Nawale <swapnilnawale18@gmail.com>
2021-08-28 19:27:13 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ForEachDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string i in args)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
